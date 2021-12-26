25e3261d33b7ae65f5c0dcaa05ca31b0803fe871
Luciano Martinez <lucianomartinez27@gmail.com>
2021-12-26 18:52:21 -0300

    related post by tag

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from blog.forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -29,12 +30,19 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+     # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+    .order_by('-same_tags','-publish')[:4]
     return render(request,
         'blog/post/detail.html',
         {'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form})
+        'comment_form': comment_form,
+        'similar_posts': similar_posts})
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -50,6 +58,7 @@ def post_list(request, tag_slug=None):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
+   
     return render(request, 'blog/post/list.html', {'page': page,
         'posts': posts,
         'tag': tag})
