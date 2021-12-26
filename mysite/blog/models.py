06a8171fd4aa6af9e4a6218fd6ee893e3b8c4fd1
Luciano Martinez <lucianomartinez27@gmail.com>
2021-12-26 17:46:48 -0300

    add tags

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -28,6 +28,7 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,
                               default='draft')
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,8 @@ from blog.models import Post, Comment
 from django.views.generic import ListView
 from blog.forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from taggit.models import Tag
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -19,7 +21,6 @@ def post_detail(request, year, month, day, post):
                                     publish__day=day)
     comments = post.comments.filter(active=True)
     new_comment = None
-
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -28,15 +29,36 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-
-
-
     return render(request,
         'blog/post/detail.html',
         {'post': post,
         'comments': comments,
         'new_comment': new_comment,
         'comment_form': comment_form})
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page,
+        'posts': posts,
+        'tag': tag})
+
+    
+
+
+
+    
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
