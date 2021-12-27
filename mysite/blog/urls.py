fdfc26f21d600b0a741a54e22d7d6fea7e7fc7bd
Luciano Martinez <lucianomartinez27@gmail.com>
2021-12-27 11:20:00 -0300

    add feed

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
 
 urlpatterns = [
@@ -11,4 +13,5 @@ urlpatterns = [
         name='post_detail'),
     #path('', views.PostListView.as_view(), name='post_list'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/',  LatestPostsFeed(), name='post_feed'),
 ]
