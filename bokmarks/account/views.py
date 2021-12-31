27f6eb99d534b7e294bf8ada26b3510c6190c236
Luciano Martinez <lucianomartinez27@gmail.com>
2021-12-30 21:55:57 -0300

    dashboard

--- bokmarks/account/views.py
+++ bokmarks/account/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
 
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section':'dashboard'})
+
 
 def user_login(request):
     if request.method == 'POST':
