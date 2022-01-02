334ea45d6a2892bfa3d80b38517bcac9376f1f1f
Luciano Martinez <lucianomartinez27@gmail.com>
2022-01-01 22:49:40 -0300

    add profile

--- bokmarks/account/urls.py
+++ bokmarks/account/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register,name= 'register'),
-
-
-
+    path('edit/', views.edit, name= 'edit'),
 ]
